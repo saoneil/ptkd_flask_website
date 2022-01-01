746490f4f9482ba235731479c54824480f6f91b5
Sean O'Neil <saoneil@live.com>
2022-01-01 13:30:18 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+#https://www.youtube.com/watch?v=2e4STDACVA8&list=PLCC34OHNcOtqJBOLjXTd5xC0e-VD3siPn
+
+app = Flask(__name__, static_folder='./static')
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/upcomingevents')
+def upcomingevents():
+	return render_template("upcomingevents.html")
+
+@app.route('/scheduleprices')
+def scheduleprices():
+	return render_template("scheduleprices.html")
+
+@app.route('/gallery')
+def gallery():
+	#return render_template("gallery.html")
+	return("this function is not ready yet")
+
+@app.route('/members')
+def members():
+	#return  render_template("members.html")
+	return("this function is not ready yet")
+
+@app.route('/information')
+def information():
+	return render_template("schedule+prices.html")
